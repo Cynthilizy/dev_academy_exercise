91da372348202294aaa50973145f4cdd864d2bb3
Cynthilizy <cynthilizy@gmail.com>
2023-03-27 22:52:34 +0300

    added check for form input fields

--- src/addTrip.js
+++ src/addTrip.js
@@ -15,7 +15,6 @@ const defaultFormData = {
 const AddTrip = () => {
   const navigate = useNavigate();
   const [addNew, setAddNew] = useState(defaultFormData);
-  const [formData, setFormData] = useState(new FormData());
   const [tripList, setTripList] = useState([]);
 
 
@@ -25,16 +24,12 @@ const AddTrip = () => {
 
   const handleAdd = async (e) => {
     e.preventDefault();
-    const newFormData = new FormData();
-      newFormData.append('Departure', addNew.Departure);
-      newFormData.append('Return_time', addNew.Return_time);
-      newFormData.append('Departure_Station_ID', addNew.Departure_Station_ID);
-      newFormData.append('Departure_Station_Name', addNew.Departure_Station_Name);
-      newFormData.append('Return_Station_ID', addNew.Return_Station_ID);
-      newFormData.append('Return_Station_Name', addNew.Return_Station_Name);
-      newFormData.append('Covered_Distance_Meters', addNew.Covered_Distance_Meters);
-      newFormData.append('Duration_Seconds', addNew.Duration_Seconds);
-    setFormData(newFormData);
+    const isAnyFieldBlank = Object.values(addNew).some((value) => value === '');
+
+    if (isAnyFieldBlank) {
+      window.alert('Please fill in all fields.');
+      return;
+    }
     try {
       console.log(addNew);
       const response = await fetch('http://localhost:3070/trip', {
@@ -68,37 +63,37 @@ const AddTrip = () => {
     <form name="formAdd" className="form">
       <label>
         <div> Departure time:</div>
-        <div> <input type="datetime-local" name="Departure" value={addNew.Departure} onChange={handleChange} /></div>
+        <div> <input type="datetime-local" name="Departure" value={addNew.Departure} onChange={handleChange}/></div>
       </label><br />
       <label>
         <div> Return time:</div>
-        <div> <input type="datetime-local" name="Return_time" value={addNew.Return_time} onChange={handleChange} /></div>
+        <div> <input type="datetime-local" name="Return_time" value={addNew.Return_time} onChange={handleChange}/></div>
       </label><br />
       <label>
         <div>Departure_Station_ID:</div>
-        <div><input type="text" name="Departure_Station_ID" value={addNew.Departure_Station_ID} onChange={handleChange} /></div>
+        <div><input type="text" name="Departure_Station_ID" value={addNew.Departure_Station_ID} onChange={handleChange}/></div>
       </label><br />
       <label>
         <div> Departure_Station_Name:</div>
-        <div> <input type="text" name="Departure_Station_Name" value={addNew.Departure_Station_Name} onChange={handleChange} /></div>
+        <div> <input type="text" name="Departure_Station_Name" value={addNew.Departure_Station_Name} onChange={handleChange}/></div>
       </label><br />
       <label>
         <div>Return_Station_ID:</div>
-        <div><input type="text" name="Return_Station_ID" value={addNew.Return_Station_ID} onChange={handleChange} /></div>
+        <div><input type="text" name="Return_Station_ID" value={addNew.Return_Station_ID} onChange={handleChange}/></div>
       </label><br />
       <label>
         <div>Return_Station_Name:</div>
-        <div><input type="text" name="Return_Station_Name" value={addNew.Return_Station_Name} onChange={handleChange} /></div>
+        <div><input type="text" name="Return_Station_Name" value={addNew.Return_Station_Name} onChange={handleChange}/></div>
       </label><br />
       <label>
         <div> Covered_Distance_Meters:</div>
-        <div> <input type="text" name="Covered_Distance_Meters" value={addNew.Covered_Distance_Meters} onChange={handleChange} /></div>
+        <div> <input type="text" name="Covered_Distance_Meters" value={addNew.Covered_Distance_Meters} onChange={handleChange}/></div>
       </label><br />
       <label>
         <div> Duration_Seconds:</div>
-        <div> <input type="text" name="Duration_Seconds" value={addNew.Duration_Seconds} onChange={handleChange} /></div>
+        <div> <input type="text" name="Duration_Seconds" value={addNew.Duration_Seconds} onChange={handleChange}/></div>
       </label><br />
-      <div><button className="addNewSaveBtn" type="submit" onClick={handleAdd} ><b>Save</b></button><button className="addNewCloseBtn" onClick={HandleClose}><b>Cancel</b></button></div>
+      <div><button className="addNewSaveBtn" type="submit" onClick={handleAdd} ><b>Save</b></button><button className="addNewCloseBtn" onClick={HandleClose}><b>Close</b></button></div>
     </form>
   );
 }
